d35632ec95bb61294798601081579070eb7431a4
raskinverma <vermaraskin@gmail.com>
2025-10-04 14:53:17 +0530

    Update faculty.py

--- timetable_automation/models/faculty.py
+++ timetable_automation/models/faculty.py
@@ -1,5 +1,7 @@
 class Faculty:
-    def __init__(self, faculty_id, name, department):
-        self.faculty_id = faculty_id
-        self.name = name
-        self.department = department
+    def __init__(self, faculty_id, name):
+        self.faculty_id = faculty_id  # e.g. "F001"
+        self.name = name              # e.g. "Dr. Abdul Wahid"
+
+    def __repr__(self):
+        return f"Faculty({self.faculty_id}, {self.name})"
