4a48e89a7f9d4052a114224968f848fdafe947e9
raskinverma <vermaraskin@gmail.com>
2025-10-04 14:50:42 +0530

    Update batches.py

--- timetable_automation/models/batches.py
+++ timetable_automation/models/batches.py
@@ -1,6 +1,10 @@
 class Batch:
-    def __init__(self, id, year, section, size):
-        self.id = id
-        self.year = year
-        self.section = section
-        self.size = size
+    def __init__(self, department, semester, total_students, max_batch_size):
+        self.department = department         # e.g. "CSE"
+        self.semester = semester             # e.g. 3
+        self.total_students = total_students # e.g. 170
+        self.max_batch_size = max_batch_size # e.g. 85
+
+    def __repr__(self):
+        return (f"Batch(Department={self.department}, Semester={self.semester}, "
+                f"Total_Students={self.total_students}, MaxBatchSize={self.max_batch_size})")
