98d59e784d85be642c1672f03f5a285cbcf98402
raskinverma <vermaraskin@gmail.com>
2025-10-04 15:49:56 +0530

    Update course.py

--- timetable_automation/models/course.py
+++ timetable_automation/models/course.py
@@ -1,12 +1,17 @@
 class Course:
-    def __init__(self, department, semester, course_code, course_name, ltp_sc, faculty):
-        self.department = department      
-        self.semester = semester            
-        self.course_code = course_code    
-        self.course_name = course_name     
-        self.ltp_sc = ltp_sc             
-        self.faculty = faculty            
+    def __init__(self, department, semester, course_code, course_name, L, T, P, S, C, faculty):
+        self.department = department   
+        self.semester = semester        
+        self.course_code = course_code  
+        self.course_name = course_name  
+        self.L = int(L)              
+        self.T = int(T)               
+        self.P = int(P)            
+        self.S = int(S)               
+        self.C = int(C)                 
+        self.faculty = faculty         
 
     def __repr__(self):
         return (f"Course({self.department}, Sem {self.semester}, {self.course_code}, "
-                f"{self.course_name}, {self.ltp_sc}, {self.faculty})")
+                f"{self.course_name}, L={self.L}, T={self.T}, P={self.P}, S={self.S}, C={self.C}, "
+                f"Faculty={self.faculty})")
