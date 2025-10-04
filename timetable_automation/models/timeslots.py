a1cc0ef0eea1b9dc333adcb5175ad0acad7030c4
raskinverma <vermaraskin@gmail.com>
2025-10-04 14:55:13 +0530

    Update timeslots.py

--- timetable_automation/models/timeslots.py
+++ timetable_automation/models/timeslots.py
@@ -1,6 +1,10 @@
 class TimeSlot:
     def __init__(self, slot_id, day, start_time, end_time):
-        self.slot_id = slot_id
-        self.day = day
-        self.start_time = start_time
-        self.end_time = end_time
+        self.slot_id = int(slot_id)
+        self.day = day                 
+        self.start_time = start_time    
+        self.end_time = end_time       
+
+    def __repr__(self):
+        return (f"TimeSlot(ID={self.slot_id}, Day={self.day}, "
+                f"{self.start_time}-{self.end_time})")
